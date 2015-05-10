cf280a3477c8036894c078ce8cb63099b8d31377
Drew DeVault <sir@cmpwn.com>
2015-05-09 20:27:45 -0600

    Add .part command

--- wormy/Modules/ChannelModule.cs
+++ wormy/Modules/ChannelModule.cs
@@ -28,10 +28,40 @@ namespace wormy.Modules
                     }
                     else
                     {
-                        Respond(e, "I already know about {0}.", arguments[0]);
+                        if (channel.Enabled)
+                            Respond(e, "I already know about {0}.", arguments[0]);
+                        else
+                        {
+                            channel.Enabled = true;
+                            session.SaveOrUpdate(channel);
+                            if (network.Client.Channels.All(c => c.Name != channel.Name))
+                                network.Client.JoinChannel(channel.Name);
+                        }
                     }
                 }
             }, "join [channel]: Adds the specified channel to the bot's channel list.");
+            RegisterAdminCommand("part", (arguments, e) =>
+            {
+                if (arguments.Length != 1) return;
+                using (var session = Program.Database.SessionFactory.OpenSession())
+                {
+                    var channel = session.Query<Channel>().SingleOrDefault(cw => cw.Name == arguments[0]);
+                    if (channel == null)
+                        Respond(e, "I don't know about that channel.");
+                    else
+                    {
+                        if (channel.Enabled)
+                            Respond(e, "This channel is already enabled {0}.", arguments[0]);
+                        else
+                        {
+                            channel.Enabled = false;
+                            session.SaveOrUpdate(channel);
+                            if (network.Client.Channels.Any(c => c.Name != channel.Name))
+                                network.Client.PartChannel(channel.Name);
+                        }
+                    }
+                }
+            }, "part [channel]: Disables a given channel.");
         }
     }
 }
